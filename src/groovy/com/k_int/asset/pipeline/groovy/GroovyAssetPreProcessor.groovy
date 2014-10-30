31a3ddd407d93a558f35b12e7a2c5bca1cd33cd7
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-10-30 16:26:53 +0000

    Changed to SimpleTextProcessor.

--- src/groovy/com/k_int/asset/pipeline/groovy/GroovyAssetPreProcessor.groovy
+++ src/groovy/com/k_int/asset/pipeline/groovy/GroovyAssetPreProcessor.groovy
@@ -1,19 +1,10 @@
 package com.k_int.asset.pipeline.groovy
 
 import grails.util.Holders
-import groovy.text.TemplateEngine
+import groovy.text.SimpleTemplateEngine
 import groovy.util.logging.Log4j
 
-import org.codehaus.groovy.grails.commons.ApplicationAttributes
 import org.codehaus.groovy.grails.commons.GrailsApplication
-import org.codehaus.groovy.grails.commons.spring.GrailsRuntimeConfigurator
-import org.codehaus.groovy.grails.plugins.DefaultGrailsPluginManager
-import org.codehaus.groovy.grails.plugins.GrailsPluginManager
-import org.codehaus.groovy.grails.web.pages.GroovyPagesTemplateEngine
-import org.codehaus.groovy.grails.web.pages.TagLibraryLookup
-import org.codehaus.groovy.grails.web.pages.ext.jsp.TagLibraryResolver
-import org.springframework.context.ApplicationContext
-import org.springframework.web.context.WebApplicationContext
 
 import asset.pipeline.AssetCompiler
 import asset.pipeline.AssetFile
@@ -21,7 +12,7 @@ import asset.pipeline.AssetFile
 @Log4j
 class GroovyAssetPreProcessor {
 
-  private static TemplateEngine engine
+  private static SimpleTemplateEngine engine
   def binding
   static def extensions = ['grass']
   
@@ -30,56 +21,18 @@ class GroovyAssetPreProcessor {
     GrailsApplication grailsApplication
     
     if (!compiler) {
-      log.debug("Constructing GSP template pre-processor.")
+      log.debug("Constructing Groovy template pre-processor.")
       grailsApplication = Holders.getGrailsApplication()
       
-      if (!engine) {
-        engine = Holders.getApplicationContext().getBean("groovyPagesTemplateEngine")
-      }
-      
     } else {
-      log.debug("Constructing GSP template pre-processor for pre-compilation.")
+      log.debug("Constructing Groovy template pre-processor for pre-compilation.")
       
       // Called from script. We read the attributes from the options passed from our event listener.
       grailsApplication = compiler.options."grailsApplication"
-      
-      
-      // We have to create an engine and should probably only do this once.
-      if (!engine) {
-       
-        def srvCtx = compiler.options."servletContext"
-        ApplicationContext appCtx = compiler.options."applicationContext"
-        DefaultGrailsPluginManager pluginManager = Holders.getPluginManager()
-        
-        pluginManager.registerProvidedArtefacts(null)
-        
-        // Configure the app. This will load all beans etc.
-        GrailsRuntimeConfigurator cfg = new GrailsRuntimeConfigurator(grailsApplication, appCtx)
-        cfg.reconfigure(appCtx, srvCtx, true)
-        
-        // Bind a mock web request...
-        grails.util.GrailsWebUtil.bindMockWebRequest(appCtx)
-        
-        // Create the tag library lookup.
-        TagLibraryLookup tll = new TagLibraryLookup()
-        tll.setApplicationContext(appCtx)
-        tll.setGrailsApplication(grailsApplication)
-        
-        // Create teh tag lib resolver.
-        TagLibraryResolver tlr = new TagLibraryResolver()
-        tlr.setGrailsApplication(grailsApplication)
-        tlr.setServletContext(srvCtx)
-        
-        log.debug ("Creating GSP Template engine.")
-        engine = new GroovyPagesTemplateEngine()
-        GroovyPagesTemplateEngine en = engine
-        en.setApplicationContext(appCtx)
-        en.setServletContext(srvCtx)
-        en.setClassLoader(compiler.options."classLoader")
-        en.setTagLibraryLookup(tll)
-        en.setJspTagLibraryResolver(tlr)
-      }
     }
+    
+    // Create the simple template engine if we haven't got one.
+    if (!engine) engine = new SimpleTemplateEngine()
     
     log.debug ("Adding bindings to pass through.")
     this.binding = [
@@ -95,10 +48,10 @@ class GroovyAssetPreProcessor {
     if (inputText && extensions.find { assetFile?.file?.getPath()?.matches("^.+\\.${it}\\..+") }) {
 
       // Attempt to process the file.
-      log.debug("Processing assetFile: ${assetFile.file.name} as GSP")
+      log.debug("Processing assetFile: ${assetFile.file.name} as Groovy text")
       StringWriter stringWriter = new StringWriter()
       try {
-        engine.createTemplate(inputText, assetFile.file.name).make(binding).writeTo(stringWriter)
+        engine.createTemplate(inputText).make(binding).writeTo(stringWriter)
         return stringWriter.toString()
       } catch (Exception e) {
         log.error("Error compiling groovy template asset file", e)
@@ -108,7 +61,7 @@ class GroovyAssetPreProcessor {
       }
     }
 
-    log.debug("GSP processor skipping assetFile: ${assetFile.file.name}")
+    log.debug("Groovy processor skipping assetFile: ${assetFile.file.name}")
     // Just return the original if we don't recognise this file.
     return inputText
   }
